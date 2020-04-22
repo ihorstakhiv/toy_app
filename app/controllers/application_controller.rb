76a7983e49519b556e81d061094e93b9370956e7
Ihor Stakhiv <ihorstakhivs@gmail.com>
2020-04-22 09:35:49 +0300

    Finish toy app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  def hello
-    render html: 'Say hello to my gun'
-  end
+  # def hello
+  #   render html: 'Say hello to my gun'
+  # end
 end
